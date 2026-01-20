ef48bb2a30eab0b912b7f13c86a801f243bc4ab2
Shahzod <shachzod2004@gmail.com>
2026-01-20 10:41:32 +0100

    Fix previous commit

--- ClientRenderer/Program.cs
+++ ClientRenderer/Program.cs
@@ -369,7 +369,7 @@ async Task RenderVideo()
                 if (renderUpdates.TryGetValue("Progress", out string? progressString) &&
                     double.TryParse(progressString, out double progress) && progress != 0)
                 {
-                    await serverConnection.ReportRenderingProgress(renderJob!.JobId, Math.Min(100, progress));
+                    await serverConnection.ReportRenderingProgress(renderJob!.JobId, Math.Min(1.0, progress));
                     Log($"[JobId:{renderJob!.JobId}] Rendering progress: {progress * 100:0.00}%");
                 }
                 await Task.Delay(1000, cancellationToken);
@@ -426,7 +426,7 @@ async Task RenderVideo()
                 if (renderUpdates.TryGetValue("Progress", out string? progressString) &&
                     double.TryParse(progressString, out double progress) && progress != 0)
                 {
-                    await serverConnection.ReportRenderingProgress(renderJob!.JobId, Math.Min(100, progress));
+                    await serverConnection.ReportRenderingProgress(renderJob!.JobId, Math.Min(1.0, progress));
                     Log($"[JobId:{renderJob!.JobId}] Rendering progress: {progress * 100:0.00}%");
                 }
                 await Task.Delay(1000, cancellationToken);
